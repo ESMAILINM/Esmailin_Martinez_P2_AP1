c56313fd63a33dc6cd41e3817719d955cf3a28cc
ESMAILINM <esmailinmartinez05@gmail.com>
2025-03-10 12:02:42 -0400

    Agregar contexto

--- Esmailin_Martinez_P2_AP1/Program.cs
+++ Esmailin_Martinez_P2_AP1/Program.cs
@@ -1,4 +1,7 @@
 using Esmailin_Martinez_P2_AP1.Components;
+using Esmailin_Martinez_P2_AP1.DAL;
+using Esmailin_Martinez_P2_AP1.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+//
+var ConSrt = builder.Configuration.GetConnectionString("SqlConStr");
+
+//Agregar contexto al builder con ConStr
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConSrt));
+
+//Inyectar del service
+builder.Services.AddScoped<Service>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
