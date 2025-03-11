3622c13b33c81a6aaa12da86f3c4be868a4e4358
ESMAILINM <esmailinmartinez05@gmail.com>
2025-03-10 22:05:56 -0400

    Crea Services

---
+++ Esmailin_Martinez_P2_AP1/Services/CiudadesService.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq.Expressions;
+using Esmailin_Martinez_P2_AP1.DAL;
+using Esmailin_Martinez_P2_AP1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Esmailin_Martinez_P2_AP1.Services
+{
+    public class CiudadesService(IDbContextFactory<Contexto> DbFactory)
+    {
+
+        //1.Metodo Guardar
+        public async Task<bool> Guardar(Ciudades ciudad)
+        {
+
+            if (!await Existe(ciudad.CiudadId))
+            {
+                return await Insertar(ciudad);
+            }
+            else
+            {
+                return await Modificar(ciudad);
+            }
+        }
+        //2.Metodo Existe
+        public async Task<bool> Existe(int Id)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Ciudades
+                .AnyAsync(T => T.CiudadId == Id);
+        }
+        //3.Insertar
+        public async Task<bool> Insertar(Ciudades ciudades)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.Ciudades.Add(ciudades);
+            return await contexto.SaveChangesAsync() > 0;
+        }
+        //4.Metodo Modificar
+        public async Task<bool> Modificar(Ciudades ciudades)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.Update(ciudades);
+            return await contexto
+                .SaveChangesAsync() > 0;
+        }
+        //5.Buscar
+        public async Task<Ciudades?> Buscar(int id)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Ciudades
+                .FirstOrDefaultAsync(c => c.CiudadId == id);
+
+        }
+        //6.Eliminar
+        public async Task<bool> Eliminar(int id)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Ciudades
+                .Where(c => c.CiudadId == id)
+                .ExecuteDeleteAsync() > 0;
+        }
+        //7.Metodo Listar
+        public async Task<List<Ciudades>> Listar(Expression<Func<Ciudades, bool>> criterio)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Ciudades
+                .Where(criterio)
+                .ToListAsync();
+        }
+    }
+}
+
+
+
---
+++ Esmailin_Martinez_P2_AP1/Services/EncuestaService.cs
@@ -0,0 +1,76 @@
+﻿using Esmailin_Martinez_P2_AP1.DAL;
+using Esmailin_Martinez_P2_AP1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Esmailin_Martinez_P2_AP1.Services
+{
+        public class EncuestaService(IDbContextFactory<Contexto> DbFactory)
+        {
+
+            //1.Metodo Guardar
+            public async Task<bool> Guardar(Encuesta encuesta)
+            {
+                if (!await Existe(encuesta.EncuestaId))
+                {
+                    return await Insertar(encuesta);
+                }
+                else
+                {
+                    return await Modificar(encuesta);
+                }
+            }
+
+            //2.Metodo Existe
+            public async Task<bool> Existe(int Id)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                return await contexto.Encuesta
+                    .AnyAsync(E => E.EncuestaId == Id);
+            }
+
+            //3.Insertar
+            public async Task<bool> Insertar(Encuesta encuesta)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                contexto.Encuesta.Add(encuesta);
+                return await contexto.SaveChangesAsync() > 0;
+            }
+
+            //4.Metodo Modificar
+            public async Task<bool> Modificar(Encuesta encuesta)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                contexto.Update(encuesta);
+                return await contexto
+                    .SaveChangesAsync() > 0;
+            }
+
+            //5.Buscar
+            public async Task<Encuesta?> Buscar(int id)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                return await contexto.Encuesta
+                    .FirstOrDefaultAsync(e => e.EncuestaId == id);
+            }
+
+            //6.Eliminar
+            public async Task<bool> Eliminar(int id)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                return await contexto.Encuesta
+                    .Where(e => e.EncuestaId == id)
+                    .ExecuteDeleteAsync() > 0;
+            }
+
+            //7.Metodo Listar
+            public async Task<List<Encuesta>> Listar(Expression<Func<Encuesta, bool>> criterio)
+            {
+                await using var contexto = await DbFactory.CreateDbContextAsync();
+                return await contexto.Encuesta
+                    .Where(criterio)
+                    .ToListAsync();
+            }
+        }
+    }
+
