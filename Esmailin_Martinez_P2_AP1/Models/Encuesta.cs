a9f19108ac8ef1a8bc033098a77d577e23ffd4f6
ESMAILINM <esmailinmartinez05@gmail.com>
2025-03-10 22:06:42 -0400

    Crea los modelos  Ciudades y Encuesta

---
+++ Esmailin_Martinez_P2_AP1/Models/Encuesta.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Esmailin_Martinez_P2_AP1.Models
+{
+    public class Encuesta
+
+    {
+        [Key]
+        public int EncuestaId { get; set; }
+        [Required(ErrorMessage = "El campo Nombre no puede estar vacío")]
+        public string Asignatura { get; set; }
+        public DateTime Fecha { get; set; }= DateTime.Now;
+
+        public int CiudadId { get; set; }
+        [ForeignKey("CiudadId")]
+        public Ciudades Ciudades { get; set; }
+    }
+}
